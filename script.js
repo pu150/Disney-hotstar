625af7766bb25ec04c947ba4b61fc438723499cc
pu150 <pundalikgidd2006@gmail.com>
2025-02-11 05:55:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple alert on Login form submission
+document.querySelector('#login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    alert('Login Successful');
+});
+
+// You can extend this JavaScript for more interactivity
